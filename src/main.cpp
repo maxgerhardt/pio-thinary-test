b806e1394210c4851d620d4cb3c61b4877c8e2d6
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2025-01-03 14:24:33 +0100

    Add test firmware

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
 void setup() {
-
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-    
+    Serial.println("Blinky!");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }
